845e488d7fc40b982926f485b54f0b14d4428f17
RickNick345 <ricards.knuipis@edu.rtu.lv>
2024-03-08 14:53:53 +0200

    commit(main)

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -3,37 +3,42 @@
 #include <map>
 using namespace std;
 
-
 class Graph
 {
 private:
-	// Структура узла графа
+	// Struct defining properties of a graph node
 	struct GraphNode
 	{
-		int id;
-		int number;
-		int level;
-		int p1_score;
-		int p2_score;
-		vector<GraphNode*> ChildNodes;
+		int id;                 // Node ID
+		int number;             // Arbitrary number associated with the node
+		int level;              // Level of the node in the graph hierarchy
+		int p1_score;           // Score associated with player 1
+		int p2_score;           // Score associated with player 2
+		vector<GraphNode*> ChildNodes; // Child nodes of this node
 
-	//конструктор узла
+		// Constructor to initialize the node
 		GraphNode(int id, int number, int level, int p1_score, int p2_score) : id(id), number(number), level(level), p1_score(p1_score), p2_score(p2_score) {}
 	};
-	//карта графа
+
+	// Map to store nodes with their IDs as keys
 	map<int, GraphNode*> nodes;
+
 public:
-	//конструктор графа
+	// Constructor
 	Graph() {}
-	//деструктор графа
+
+	// Destructor
 	~Graph();
-	//метод добавляющий в карту графа новый узел (вызывается методом generateGraph())
+
+	// Function to add a new node to the graph
 	void addNode(int id, int number, int level, int p1_score, int p2_score);
-	//метод соеденяет узел графа(srcId) с узлом(endId)
+
+	// Function to add an edge between two nodes in the graph
 	void addEdge(int srcId, int endId);
-	//метод выводит в консоль узлы графа(debug)
+
+	// Function to print information about all nodes in the graph
 	void printNodes();
-	//метод создает граф принимая начальное число
+
+	// Function to generate a graph starting from a specified number
 	void generateGraph(int startNum);
 };
-
