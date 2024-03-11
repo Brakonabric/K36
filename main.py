ccf18d7d223782693d12ef7619ecee641a63de81
Linass Joksass <dizzneyk@gmail.com>
2024-03-11 19:46:22 +0200

    gui menu

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from tkinter import *
 import tkinter as ttk
-from tkinter import messagebox
-from style import
+from tkinter import PhotoImage
 
 root = Tk()  # Primāra loga inicijalizacija ar nosaukumu root.
-root['bg'] = '#2a374a'  # Primāra loga fona krāsa.
 root.title('K36 GAMES')
 root.geometry('800x600')  # Primāra loga izmērs.
 root.resizable(width=False, height=False)
 
-rootHeader = Canvas(root, MainMenu.start() ).place(x=300, y=242)
-
+bg = PhotoImage(file='assets/main_menu_background.png')
+background = Canvas(root, width=800, height=600)
+background.pack()
+background.create_image(0, 0, image=bg, anchor=NW)
 root.mainloop()
