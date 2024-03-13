6cf74af4f851a54e57df8422fdd8586a2fa197df
Linass Joksass <dizzneyk@gmail.com>
2024-03-13 19:48:00 +0200

    100% work buttons

--- main.py
+++ main.py
@@ -12,26 +12,6 @@ def on_click_exit():
     root.destroy()
 
 
-def change_player():
-    FirstPlayer.change_player(False)
-    if FirstPlayer.current_player() == "human":
-        who_starts_button.config(image=Assets.preset_human_img)
-    elif FirstPlayer.current_player() == "ai":
-        who_starts_button.config(image=Assets.preset_ai_img)
-    else:
-        return
-
-
-def change_alg():
-    UseAlgorithm.change_alg(False)
-    if UseAlgorithm.current_alg() == "Alfa-beta":
-        alg_button.config(image=Assets.preset_alg_ab_img)
-    elif UseAlgorithm.current_alg() == "Minimax":
-        alg_button.config(image=Assets.preset_alg_mm_img)
-    else:
-        return
-
-
 def about(mode):
     def close_about():
         about_fake_button.destroy()
@@ -68,6 +48,19 @@ def game_menu(turn, mode, start):
 
 
 def finish_menu(mode):
+    def to_preset_menu():
+        background.create_image(0, 0, image=Assets.preset_back_img, anchor=NW)
+        start_again_button.destroy()
+        exit_button.destroy()
+        menu_bar.destroy()
+        preset_menu()
+
+    def to_main():
+        start_again_button.destroy()
+        menu_bar.destroy()
+        exit_button.destroy()
+        main_menu()
+
     if mode == "human":
         background.create_image(0, 0, image=Assets.final_victory_bg_img, anchor=NW)
     elif mode == "ai":
@@ -75,9 +68,9 @@ def finish_menu(mode):
     else:
         background.create_image(0, 0, image=Assets.final_draw_bg_img, anchor=NW)
 
-    exit_button = Button(root, image=Assets.final_exit_img, border=0, command=on_click_exit)
-    start_again_button = Button(root, image=Assets.final_start_img, border=0, command=lambda: {})
-    menu_bar = Button(root, image=Assets.final_menu_img, border=0, command=lambda: {})
+    exit_button = Button(root, image=Assets.final_exit_img, border=0, command=lambda: on_click_exit())
+    start_again_button = Button(root, image=Assets.final_start_img, border=0, command=lambda: to_preset_menu())
+    menu_bar = Button(root, image=Assets.final_menu_img, border=0, command=lambda: to_main())
 
     start_again_button.place(x=500, y=270)
     menu_bar.place(x=500, y=330)
@@ -85,9 +78,12 @@ def finish_menu(mode):
 
 
 def preset_menu():
+    first_player = WhoPlayFirst()
+    used_algorithm = UsingAlgorithm()
+
     def check_rules():
-        player_state = FirstPlayer.current_player()
-        alg_state = UseAlgorithm.current_alg()
+        player_state = first_player.current_player()
+        alg_state = used_algorithm.current_alg()
         if player_state is not None and alg_state is not None:
             start_button.destroy()
             alg_button.destroy()
@@ -102,9 +98,29 @@ def preset_menu():
             print(player_state)
             print(alg_state)
 
+    def change_player():
+        first_player.change_player(False)
+        if first_player.current_player() == "human":
+            who_starts_button.config(image=Assets.preset_human_img)
+        elif first_player.current_player() == "ai":
+            who_starts_button.config(image=Assets.preset_ai_img)
+        else:
+            return
+
+    def change_alg():
+        used_algorithm.change_alg(False)
+        if used_algorithm.current_alg() == "Alfa-beta":
+            alg_button.config(image=Assets.preset_alg_ab_img)
+        elif used_algorithm.current_alg() == "Minimax":
+            alg_button.config(image=Assets.preset_alg_mm_img)
+        else:
+            return
+
     start_button = Button(root, image=Assets.main_menu_start_img, border=0, command=check_rules)
     start_button.place(x=275, y=263)
+    alg_button = Button(root, image=Assets.preset_algorithm_img, border=0, command=lambda: change_alg())
     alg_button.place(x=502, y=352)
+    who_starts_button = Button(root, image=Assets.preset_who_starts_img, border=0, command=lambda: change_player())
     who_starts_button.place(x=150, y=352)
     input_box = Canvas(root, width=100, height=50, highlightthickness=0)
     input_box.create_image(0, 0, image=Assets.preset_number_img, anchor=NW)
@@ -112,8 +128,7 @@ def preset_menu():
     input_line = Entry(input_box, width=2, border=0, justify="center", font=('Terminal', 25, 'bold'))
     input_line.place(x=24, y=8)
     about_button = Button(root, image=Assets.main_menu_about_img, border=0, background='white',
-                          activebackground='white',
-                          command=lambda: about("preset"))
+                          activebackground='white', command=lambda: about("preset"))
     about_button.place(x=375, y=485)
 
 
@@ -126,20 +141,16 @@ def main_menu():
         preset_menu()
 
     background.create_image(0, 0, image=Assets.main_menu_bg_img, anchor=NW)
-    start_button = Button(root, image=Assets.main_menu_start_img, border=0, command=to_preset_menu)
+    start_button = Button(root, image=Assets.main_menu_start_img, border=0, command=lambda: to_preset_menu())
     start_button.place(x=275, y=263)
-    exit_button = Button(root, image=Assets.main_menu_exit_img, border=0, command=on_click_exit)
+    exit_button = Button(root, image=Assets.main_menu_exit_img, border=0, command=lambda: on_click_exit())
     exit_button.place(x=300, y=350)
     about_button = Button(root, image=Assets.main_menu_about_img, border=0, background='white',
-                          activebackground='white',
-                          command=lambda: about("main"))
+                          activebackground='white', command=lambda: about("main"))
     about_button.place(x=375, y=485)
 
 
 # Main but not def
-FirstPlayer = WhoPlayFirst()
-UseAlgorithm = UsingAlgorithm()
-
 root = Tk()
 root.title('K36 GAMES')
 root.geometry('800x600')
@@ -148,8 +159,5 @@ from data.assets import LoadAssets as Assets
 
 background = Canvas(root, width=800, height=600)
 background.pack()
-who_starts_button = Button(root, image=Assets.preset_who_starts_img, border=0, command=change_player)
-alg_button = Button(root, image=Assets.preset_algorithm_img, border=0, command=change_alg)
-
 main_menu()
 root.mainloop()
