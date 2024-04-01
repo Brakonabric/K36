c9937fc3e5ddcce0389ed360aa112cc635500ee5
Linass Joksass <dizzneyk@gmail.com>
2024-04-01 19:34:56 +0300

    Update play_game.py

--- algorithm/play_game.py
+++ algorithm/play_game.py
@@ -1,57 +1,68 @@
+import random
 import time
-from Graph.partial_graph import minimax, alphabeta
 
-defaultColor = '\033[0m'
-titleColor = '\x1b[38;5;242m'
-timeColor = '\x1b[38;5;181m'
+from Graph.partial_graph import minimax, alphabeta
 
 
 class Game:
-    def __init__(self, alg, score, root):
+    def __init__(self, turn, alg, score, root):
         self.root = root
         self.game_score = score
+        self.play_now = turn
         self.alg = alg
+        self._current_ai_mult = 3
         self._current_human_mult = None
-        self.human_score = 0
-        self.ai_score = 0
+        self._human_score = 0
+        self._ai_score = 0
+        self._iter = 0
+
+    def get_iter(self):
+        self._iter += 1
+        return self._iter
 
     def human_turn(self, value):
-        before = self.game_score
         self._current_human_mult = value
-        self.game_score = self.game_score * self._current_human_mult
-        if self.game_score % 2 == 0:
-            self.human_score += 1
-        else:
-            self.human_score -= 1
+        print(f"    HUMAN: X{self._current_human_mult}")
 
-        print(
-            f"\n\t\tHUMAN: X{self._current_human_mult}"
-            f"\n\t\t\tDATA: {before} -> {self.game_score}"
-            f"\n\n\t{titleColor}[ HUMAN: {self.human_score} | SCORE: {self.game_score} | AI: {self.ai_score} ]{defaultColor}"
-        )
+    def get_data(self, whose_turn):
+        before = self.game_score
+        if whose_turn == "human":
+            self.game_score = self.game_score * self._current_human_mult
+            if self.game_score % 2 == 0:
+                self._human_score += 1
+            else:
+                self._human_score -= 1
+        elif whose_turn == "ai":
+            if self.play_now == "human":
+                player1 = self._human_score
+                player2 = self._ai_score
+                self._human_score,  = self.ai_turn(player1, player2)
 
-    def ai_turn(self):
+            else:
+                player2 = self._human_score
+                player1 = self._ai_score
+                self.ai_turn(player1, player2)
+
+
+            # # ВЫЗОВ класса для получения объекта вершины
+            # self.game_score = self.game_score * self._current_ai_mult # = счёт игры из вершины
+            # if self.game_score % 2 == 0: # УДАЛИТЬ ИФ
+            #     self._ai_score += 1      # УДАЛИТЬ ИФ
+            # else:                        # УДАЛИТЬ ИФ
+            #     self._ai_score -= 1 # = счёт ИИ из вершины
+
+        print(f"        DATA: {before} -> {self.game_score}")
+        return self.game_score, self._human_score, self._ai_score
+
+    def ai_turn(self, player1, player2):
         self.root.update()
-        old_score = self.game_score
-        # Алгоритм работает слишком быстро,
-        # поэтому перед его выполнением мы делаем задержку в пол секунды,
-        # чтобы GUI успел отобразить счёт игры перед ходом ИИ
-        time.sleep(0.65)
-        start_time = time.perf_counter()
-        result = None
-        if self.alg == "MINIMAX":
-            result = minimax(self.game_score, self.ai_score, self.human_score)
-        elif self.alg == "ALPHA-BETA":
-            result = alphabeta(self.game_score, self.ai_score, self.human_score)
-        end_time = time.perf_counter()
-        self.game_score = result.number
-        self.ai_score = result.p1_score
-        print(
-            f"\n\t\tAI: X{int(self.game_score / old_score)}"
-            f"\t{timeColor}TIME: {round((end_time - start_time) * 1000, 5)} ms{defaultColor}"
-            f"\n\t\t\tDATA: {old_score} -> {self.game_score}"
-            f"\n\n\t{titleColor}[ HUMAN: {result.p2_score} | SCORE: {result.number} | AI: {result.p1_score} ]{defaultColor}"
-        )
+        if self.alg == "Minimax":
+            return minimax(self.game_score, player1, player2)
 
-    def get_data(self):
-        return self.game_score, self.human_score, self.ai_score
+        elif self.alg == "Alfa-beta":
+            return alphabeta(self.game_score, player1, player2)
+
+
+        # time.sleep(0.5)
+        # self._current_ai_mult = random.randint(2, 3)
+        # print(f"    AI: X{self._current_ai_mult}")
